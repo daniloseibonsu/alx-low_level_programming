cecabdc6ab38f520756f1d2d8c43c5251813f0ce
Daniel Osei Bonsu <daniloseibonsu@gmail.com>
2023-07-07 15:41:53 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
